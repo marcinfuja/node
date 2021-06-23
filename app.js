1b5a0eafdac21a02c73420fa86fec6250c28c222
Marcin Fuja <mfuja@gorillagroup.com>
2021-06-23 13:00:37 +0200

    remove progress and add assigment 1

--- app.js
+++ app.js
@@ -1,6 +1,48 @@
 const http = require('http');
-const routes = require('./routes');
 
-const server = http.createServer(routes);
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.write(`
+            <html>
+                <body>
+                    <h1>Hello<h2>
+                    <form action="/create-user" method="POST">
+                        <input type="text" name="userName" />
+                        <button type="submit">Send</button>
+                    </form>
+                </body>
+            </html>
+        `)
+    }
+    if (req.url === '/users') {
+        res.write(`
+            <html>
+                <body>
+                    <ul>
+                        <li>User 1</li>
+                        <li>User 2</li>
+                        <li>User 3</li>
+                    </ul>
+                </body>
+            </html>
+        `)
+    }
+    if (req.url === '/create-user') {
+        const body = [];
+
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        });
+
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            console.log(message);
+            res.end();
+        });
+    }
+    res.end();
+});
 
 server.listen(3000);
