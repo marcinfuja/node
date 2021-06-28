b9cc42fa3db695bd6ee389273e43dd4e3489c5c0
Marcin Fuja <mfuja@gorillagroup.com>
2021-06-28 12:56:48 +0200

    setup for next chapter

--- app.js
+++ app.js
@@ -1,48 +1,9 @@
 const http = require('http');
 
+const routes = require('./routes');
 
-const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.write(`
-            <html>
-                <body>
-                    <h1>Hello<h2>
-                    <form action="/create-user" method="POST">
-                        <input type="text" name="userName" />
-                        <button type="submit">Send</button>
-                    </form>
-                </body>
-            </html>
-        `)
-    }
-    if (req.url === '/users') {
-        res.write(`
-            <html>
-                <body>
-                    <ul>
-                        <li>User 1</li>
-                        <li>User 2</li>
-                        <li>User 3</li>
-                    </ul>
-                </body>
-            </html>
-        `)
-    }
-    if (req.url === '/create-user') {
-        const body = [];
+console.log(routes.someText);
 
-        req.on('data', (chunk) => {
-            body.push(chunk);
-        });
-
-        req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            console.log(message);
-            res.end();
-        });
-    }
-    res.end();
-});
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
