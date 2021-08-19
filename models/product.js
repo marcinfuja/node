4f0bddc46b308758d193c9d754fc9f47134175d1
Marcin Fuja <mfuja@gorillagroup.com>
2021-08-19 15:16:44 +0200

    mvc model ending with refactor

---
+++ models/product.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+    path.dirname(require.main.filename),
+    'data',
+    'products.json'
+);
+
+const getProductsFromFile = (callback) => {
+
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            return callback([]);
+        }
+        callback(JSON.parse(fileContent));
+    });
+}
+
+module.exports = class Product {
+    constructor(t) {
+        this.title = t;
+    }
+
+    save() {
+        getProductsFromFile(products => {
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), (err) => {
+                console.log(err)
+            });
+        });
+
+        fs.readFile(p, (err, fileContent) => {
+        });
+    }
+    
+    static fetchAll(callback) {
+        getProductsFromFile(callback);
+    }
+};
