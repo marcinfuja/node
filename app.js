3f2bb334819ad666ae13e3768974fa3f6ce7faa3
Marcin Fuja <mfuja@gorillagroup.com>
2021-08-19 11:20:30 +0200

    router basics

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-
 const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product', (req, res, next) => {
-    console.log('In another middleware');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.post('/product', (req, res, next) => {
-    console.log(req.body)
-    res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-    console.log('In another middleware');
-    res.send('<h1>Hello from express</h1>')
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
 });
 
 app.listen(3000);
