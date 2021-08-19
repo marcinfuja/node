70cbd9e3350024d0a9bb4af6f38fbc87cbfac428
Marcin Fuja <mfuja@gorillagroup.com>
2021-08-19 11:08:07 +0200

    express js basics

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const routes = require('./routes');
+const app = express();
 
-console.log(routes.someText);
+app.use(bodyParser.urlencoded({extended: false}));
 
-const server = http.createServer(routes.handler);
+app.use('/add-product', (req, res, next) => {
+    console.log('In another middleware');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
+});
 
-server.listen(3000);
+app.post('/product', (req, res, next) => {
+    console.log(req.body)
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+    console.log('In another middleware');
+    res.send('<h1>Hello from express</h1>')
+});
+
+app.listen(3000);
