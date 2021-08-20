298357ad7245dd453275136c5cc15c06eb124928
Marcin Fuja <mfuja@gorillagroup.com>
2021-08-20 11:13:39 +0200

    synchronize

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,17 @@
 const path = require('path');
+
 const express = require('express');
 
-const router = express.Router();
-const productsController = require('../controllers/products');
+const shopController = require('../controllers/shop');
 
-router.get('/', productsController.getProducts);
+const router = express.Router();
+
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/cart', shopController.getCart);
+
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
